b828ac04e5e2e5625af0cf891974d6b4afd577ec
songganam <dev.ganam@gmail.com>
2024-01-08 17:26:34 +0900

    로그인 페이지 구조 잡는 중..

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -5,17 +5,20 @@ import { useNavigate } from "react-router-dom";
 // 로그인 페이지입니다.
 const LoginPage = () => {
   const navigate = useNavigate();
-
+  
   const handleClickLogin = () => {
     console.log("로그인이 되었습니다.");
   };
   const handleClickJoin = () => {
     navigate("/join");
   };
-
+  
   return (
-    <Layout>
-      <h1>로그인</h1>
+    <Layout >
+      <input className="login-id" placeholder="이메일아이디"></input>
+      <br />
+      <input className="login-pw" placeholder="비밀번호"></input>
+      <br/>
       <button
         onClick={() => {
           handleClickLogin();
@@ -23,6 +26,7 @@ const LoginPage = () => {
       >
         로그인
       </button>
+      <br />
       <button
         onClick={() => {
           handleClickJoin();
