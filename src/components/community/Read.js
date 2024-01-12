62bca59fa6269a5a98f228c1ec7bfe86240130a8
joona <dev.joona0306@gmail.com>
2024-01-13 01:14:45 +0900

    design: 고기잡담 글 상세보기 퍼블리싱 중

--- src/components/community/Read.js
+++ src/components/community/Read.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Button from "../button/Button";
 import More from "./More";
 import { WrapStyle } from "./styles/ListStyle";
 import {
-  MoreHeadStyle,
+  BtnBox,
+  BtnBoxStyle,
   MoreTitleStyle,
+  PrnvContentStyle,
   TitleBoxStyle,
+  WriterBoxStyle,
 } from "./styles/ReadStyle";
 
 const Read = () => {
@@ -14,19 +18,58 @@ const Read = () => {
         <MoreTitleStyle>
           고기로 너무 좋아요!! 중구에 고깃집 하나 추천합니다.
         </MoreTitleStyle>
-        <div className="writerBox">
+        <WriterBoxStyle>
           <div className="userName">어쭈구리고기봐라</div>
           <div className="date">2024.01.08 오후 10:30:47</div>
           <div className="viewBox">
             <img
               src={`${process.env.PUBLIC_URL}/assets/images/view_eye.svg`}
-              alt=""
+              alt="img"
             />
             <div className="viewCount">3574</div>
           </div>
-        </div>
+        </WriterBoxStyle>
       </TitleBoxStyle>
       <More />
+      <PrnvContentStyle>
+        <div className="prnv">
+          <div className="prnvIcon">
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/images/mingcute_up-line.svg`}
+              alt="img"
+            />
+          </div>
+          <div className="prnvText">이전글</div>
+        </div>
+        <div className="prnvTitle">
+          고기로 너무 좋아요!! 중구에 고깃집 하나 추천합니다.
+        </div>
+      </PrnvContentStyle>
+      <PrnvContentStyle>
+        <div className="prnv">
+          <div className="prnvIcon">
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/images/mingcute_down-line.svg`}
+              alt="img"
+            />
+          </div>
+          <div className="prnvText">다음글</div>
+        </div>
+        <div className="prnvTitle">
+          고기로 너무 좋아요!! 중구에 고깃집 하나 추천합니다.
+        </div>
+      </PrnvContentStyle>
+      <BtnBoxStyle>
+        <div className="editBtn">
+          <Button bttext="수정하기" />
+          <Button bttext="삭제하기" />
+        </div>
+        <Button bttext="목록보기" />
+      </BtnBoxStyle>
+      <div className="reviewBox">
+        <div className="readReviewBox"></div>
+        <div className="inputReviewBox"></div>
+      </div>
     </WrapStyle>
   );
 };
--- src/components/community/styles/ReadStyle.js
+++ src/components/community/styles/ReadStyle.js
@@ -11,12 +11,15 @@ export const MoreBoxStyle = styled.div`
 
 export const TitleBoxStyle = styled.div`
   position: relative;
-  display: flex;
-  gap: 10px;
-  justify-content: space-between;
   padding: 30px 20px 20px 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 10px;
+  align-self: stretch;
   font-family: "DAEAM_LEE_TAE_JOON";
   font-size: ${FontSize.strong};
+  border-bottom: 1px solid ${ColorStyle.g500};
   @media (max-width: 980px) {
     display: none;
   }
@@ -31,4 +34,53 @@ export const MoreTitleStyle = styled.div`
 export const MoreStyle = styled.div`
   position: relative;
   color: ${ColorStyle.g600};
+`;
+
+export const WriterBoxStyle = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 20px;
+  color: ${ColorStyle.g700};
+  font-size: ${FontSize.default};
+  .viewBox {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+`;
+
+export const PrnvContentStyle = styled.div`
+  position: relative;
+  display: flex;
+  padding: 20px;
+  align-items: flex-start;
+  gap: 60px;
+  align-self: stretch;
+  border-top: 1px solid ${ColorStyle.g500};
+  font-family: "DAEAM_LEE_TAE_JOON";
+  font-size: ${FontSize.strong};
+  .prnv {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+  }
+  .prnvTitle {
+    display: flex;
+    align-items: flex-start;
+    color: ${ColorStyle.g700};
+  }
+`;
+
+export const BtnBoxStyle = styled.div`
+  display: flex;
+  padding: 30px 0px 50px 0px;
+  justify-content: space-between;
+  align-items: flex-start;
+  align-self: stretch;
+  .editBtn {
+    display: flex;
+    align-items: flex-start;
+    gap: 10px;
+    flex: 1 0 0;
+  }
 `;
