5d91226bc3486666481624a10dbd5a84bc7763f8
songganam <dev.ganam@gmail.com>
2024-01-12 17:59:21 +0900

    design : JoinPage 구조 ,css 작업

--- src/pages/join/JaddPage.js
+++ src/pages/join/JaddPage.js
@@ -1,7 +1,8 @@
 import React from "react";
-import {
-  useNavigate
-} from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import TitleHeader from "../../components/titleheader/TitleHeader";
+import "../join/JaddPage.css";
+import { DefaultBt } from "../../components/button/styles/ButtonStyle";
 
 // 회원가입 작성 페이지입니다.
 const JaddPage = () => {
@@ -16,27 +17,72 @@ const JaddPage = () => {
   };
 
   return (
-    <div className="JoinPage-main">
-      <div className="JoinPage-img"></div>
-      <div className="JoinPage-write">
-        <h2>회원가입 정보 작성하기</h2>
+    <div className="JoinPage-wrap">
+      <TitleHeader
+        timg="https://picsum.photos/1920/215/?category=meat"
+        tname="회원가입"
+        tcontent="오늘도 맛있는 고기와 함께하세요"
+      ></TitleHeader>
+      <div className="JoinPage-main">
+        <div className="JoinPage-img">
+          <button className="JoinPage-img-button"></button>
+        </div>
+        <div className="JoinPage-info">
+          <div>
+            <label>이메일</label>
+            <input
+              className="Join-mail"
+              placeholder="@까지 정확하게 입력하세요."
+            ></input>
+            <br />
+            <label>이름</label>
+            <input
+              className="Join-name"
+              placeholder="본인 이름을 입력하세요."
+            ></input>
+            <br />
+            <label>비밀번호</label>
+            <input
+              className="Join-pw"
+              placeholder="비밀번호를 입력하세요.(특수문자 포함 4-8자)"
+            ></input>
+            <br />
+            <label>비밀번호 확인</label>
+            <input
+              className="Join-morepw"
+              placeholder="입력한 비밀번호를 한번 더 확인하세요."
+            ></input>
+            <br />
+            <div className="Join-gender">성별</div>
+            <br />
+            <label>닉네임</label>
+            <input
+              className="Join-nickname"
+              placeholder="사용할 닉네임을 입력하세요."
+            ></input>
+            <br />
+            <label>휴대폰 번호</label>
+            <input
+              className="Join-phonenumber"
+              placeholder="휴대폰 번호를 입력하세요."
+            ></input>
+            <br />
+            <label>주소</label>
+            <input
+              className="Join-address"
+              placeholder="거주 중인 주소를 입력하세요."
+            ></input>
+            <div className="Join-buttons">
+              <DefaultBt className="Join-button">회원가입</DefaultBt>
+              <button className="cancel-button">취소하기</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Outlet />
+        </div>
       </div>
-      <div className="JoinPage-bt">
-        <button
-          onClick={() => {
-            handleClickJoin();
-          }}
-        >
-          회원가입 작성완료
-        </button>
-        <button
-          onClick={() => {
-            handleClickCancel();
-          }}
-        >
-          회원가입 취소하기
-        </button>
-      </div>
+      <div className="JoinPage-footer"></div>
     </div>
   );
 };
--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -3,19 +3,18 @@ import { useNavigate } from "react-router-dom";
 import Layout from "../../layouts/Layout";
 import "../login/LoginPage.css";
 import TitleHeader from "../../components/titleheader/TitleHeader";
-import Button from "../../components/button/Button";
+
 
 // 로그인 페이지입니다.
 const LoginPage = () => {
-  const navigate = useNavigate();
 
+  const navigate = useNavigate();
   const handleClickLogin = () => {
     console.log("로그인이 되었습니다.");
   };
   const handleClickJoin = () => {
     navigate("/join");
   };
-
   return (
     <Layout>
       <div className="LoginPage-wrap">
@@ -24,45 +23,36 @@ const LoginPage = () => {
           tname="로그인"
           tcontent="우울할 땐 고기 앞으로"
         ></TitleHeader>
-
         <div className="LoginPage-main">
           <div className="LoginPage-info">
             <input className="Login-id" placeholder="이메일 아이디"></input>
-
             <input className="Login-pw" placeholder="비밀번호"></input>
           </div>
-
           <div className="Login-checkbox">
             <img src="../assets/images/Checkboxes.svg"></img>
             로그인 상태 유지
           </div>
           <div className="Login-bts">
-         
-       
-              <button
-            
-                className="Loginbutton"
-                onClick={() => {
-                  handleClickLogin();
-                }}
-              >
-                로그인
-              </button>
-         
-            
-              <button
-                type="button"
-                className="Joinbutton"
-                onClick={() => {
-                  handleClickJoin();
-                }}
-              >
-                회원가입
-              </button>
-            
+            <button
+              className="Loginbutton"
+              onClick={() => {
+                handleClickLogin();
+              }}
+            >
+              로그인
+            </button>
+            <button
+              type="button"
+              className="Joinbutton"
+              onClick={() => {
+                handleClickJoin();
+              }}
+            >
+              회원가입
+            </button>
           </div>
         </div>
-          <div className="LoginPage-footer"> </div>
+        <div className="LoginPage-footer"> </div>
       </div>
     </Layout>
   );
