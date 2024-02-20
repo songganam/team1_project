1ba58ab3f83aa2aa8713ecd44f8dae9ad3839cf7
songganam <dev.ganam@gmail.com>
2024-02-20 18:02:37 +0900

    design : 신고 관리, 계정 관리

--- src/pages/supervisor/styles/SupervisorReportStyle.js
+++ src/pages/supervisor/styles/SupervisorReportStyle.js
@@ -1,17 +1,51 @@
 import styled from "@emotion/styled";
 
-export const SupervisorReportWrapper = styled.div`
+// export const SupervisorReportWrapper = styled.div`
+//   position: relative;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: flex-start;
+//   gap: 17px;
+//   font-family: Pretendard;
+// `;
+
+export const SvisorReportWrap = styled.div`
   position: relative;
-  background-color: yellow;
+  background-color: #ffffff;
+  width: 1710px;
 `;
+
 export const SupervisorReportHeader = styled.div`
   position: relative;
-  background-color: red;
-  width: 1710px;
-  height: 59px;
+  display: flex;
+  width: 100%;
+  padding: 11px 36px;
+  justify-content: space-between;
+  align-items: center;
+  background: #fff;
+  /* Shadow */
+  box-shadow: 0px 1px 2px -1px rgba(0, 0, 0, 0.1),
+    0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+
+  .page-title {
+    display: flex;
+    width: 136px;
+    justify-content: space-between;
+    align-items: center;
+    flex-shrink: 0;
+    color: var(--grayscale-90, #1c1c1e);
+
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+  }
 `;
-export const SupervisorReportMain = styled.div`
-  position: relative;
-  background-color: aqua;
+export const SvisorReportMain = styled.div`
+  /* display: flex;
+  justify-content: center; */
+ 
+`;
+export const SvisorTable = styled.div`
   width: 1180px;
 `;
