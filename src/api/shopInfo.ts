4e9d10863f68818d91137f363394bfaa5fc4b30a
joona0306 <dev.joona0306@gmail.com>
2024-02-27 17:58:45 +0900

    feat: api 작성중

---
+++ src/api/shopInfo.ts
@@ -0,0 +1,67 @@
+import axios from "axios";
+import { API_SERVER_HOST } from "./config";
+
+const host = `${API_SERVER_HOST}/api/owner`;
+
+interface ShopInfo {
+  pics: string[];
+  dto: {
+    imeat: number;
+    name: string;
+    location: string;
+    open: string;
+    tel: string;
+    x: string;
+    y: string;
+    deposit: number;
+  };
+}
+
+interface MenuInfo {
+  checkShop: number;
+  imenu: number;
+  ishop: number;
+  price: number;
+  menu: string;
+  pic: string;
+}
+
+// 매장정보 수정하기
+export const putShopInfo = async (
+  shopInfo: ShopInfo,
+): Promise<ShopInfo | undefined> => {
+  try {
+    const header = { headers: { "Content-Type": "multipart/form-data" } };
+    const response = await axios.put(`${host}/modify`, shopInfo, header);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return undefined;
+  }
+};
+
+// 메뉴 리스트
+export const getMenus = async (
+  menuInfo: MenuInfo[],
+): Promise<MenuInfo[] | undefined> => {
+  try {
+    const header = { headers: { "Content-Type": "application/json" } };
+    const response = await axios.get(`${host}/menu`);
+    return response.data;
+  } catch (error) {
+    console.log("메뉴정보 호출 오류");
+    throw error;
+  }
+};
+
+// 메뉴 등록
+export const postMenu = async ({ menuInfo }: { menuInfo: FormData }) => {
+  try {
+    const header = { headers: { "Content-Type": "multipart/form-data" } };
+    const response = await axios.post(`${host}`, menuInfo, header);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
