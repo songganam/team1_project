9e52cc58ebcb7bdadc61f5c496b5ab553a1b1220
songganam <dev.ganam@gmail.com>
2024-02-29 01:09:07 +0900

    feat : userApi.js/ SupervisorUserPage.js [API 연동]

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,20 @@
 import authAxios from "../util/tokenUtil";
 import { API_SERVER_HOST } from "./config";
+const host = `${API_SERVER_HOST}/api/admin`;
 
-export const getUser = async () => {
+export const getUser = async ({ successFn, failFn, errorFn }) => {
   try {
-    const response = await authAxios.get(`${API_SERVER_HOST}/api/admin/black`);
-    return response.data;
+    const response = await authAxios.get(`${host}/black`);
+    const status = response.status.toString();
+    // return response.data;
+    if (status.charAt(0) === "2") {
+      console.log("신고글 호출 성공");
+      successFn(response.data);
+    } else {
+      failFn("신고글 호출 오류");
+    }
   } catch (error) {
-    console.log(error);
+    errorFn(error);
+    console.log("서버 오류")
   }
-}
+};
