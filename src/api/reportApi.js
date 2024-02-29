1c8ca39ed04fba3b1eae51ac3f37766d3a9708dc
songganam <dev.ganam@gmail.com>
2024-02-29 18:08:20 +0900

    feat : 리엑트 테이블 삭제

--- src/api/reportApi.js
+++ src/api/reportApi.js
@@ -1,33 +1,21 @@
 import authAxios from "../util/tokenUtil";
 import { API_SERVER_HOST } from "./config";
 
-// 내용 가져오기
-// export const getReport = async (page, row_count, bookmark, fn) => {
-//   // axios 를 사용하는 경우에는 언제든 에러가 발생할 수있다.
-//   // 반드시 try { 위험한 코드 } catch(error){} 를 사용해야한다.
-//   try {
-//     const url = `${API_SERVER_HOST}/api/admin/report/{check}`;
-//     // console.log(url);
-//     const res = await axios.get(url);
-//     fn(res.data);
-//   } catch (error) {
-//     // 개발 중에만 활용
-//     // 실제 서비스에서는 경고창으로 마무리 하자.
-//     alert(`${error} 가 발생했습니다. 데모데이터 쓸게요`);
+const svisorhost = `${API_SERVER_HOST}/api/admin`;
 
-//     // 데모 데이터를 이용해서 작업은 진행
-//     // const demo = await axios.get("getmeal.json");
-//     // fn(demo.data);
-//   }
-// };
-
-export const getReport = async ({ check }) => {
+export const getReport = async ({ successFn, failFn, errorFn }) => {
   try {
-    const response = await authAxios.get(
-      `${API_SERVER_HOST}/api/admin/report/${check}`,
-    );
-    return response.data;
+    const response = await authAxios.get(`${svisorhost}/report?check=0&page=1`);
+    const status = response.status.toString();
+
+    if (status.charAt(0) === "2") {
+      console.log("신고 관리 호출 성공");
+      successFn(response.data);
+    } else {
+      failFn("신고 관리 호출 오류");
+    }
   } catch (error) {
-    console.log(error);
+    errorFn(error);
+    console.log("신고 관리 서버 오류");
   }
 };
--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,20 +1,20 @@
 import authAxios from "../util/tokenUtil";
 import { API_SERVER_HOST } from "./config";
-const host = `${API_SERVER_HOST}/api/admin`;
+const svisorhost = `${API_SERVER_HOST}/api/admin`;
 
 export const getUser = async ({ successFn, failFn, errorFn }) => {
   try {
-    const response = await authAxios.get(`${host}/black`);
+    const response = await authAxios.get(`${svisorhost}/black?page=1`);
     const status = response.status.toString();
     // return response.data;
     if (status.charAt(0) === "2") {
-      console.log("신고글 호출 성공");
+      console.log("유저 관리 호출 성공");
       successFn(response.data);
     } else {
-      failFn("신고글 호출 오류");
+      failFn("유저 관리 호출 오류");
     }
   } catch (error) {
     errorFn(error);
-    console.log("서버 오류")
+    console.log("유저 관리 서버 오류");
   }
 };
